77a25563796d01ef73325e838ff439e1951c9c18
MahRukh <mahrukhdev@gmail.com>
2024-01-24 06:31:21 +0500

    learn

---
+++ Main.py
@@ -0,0 +1,15 @@
+import math
+
+msg = "hello"
+print(msg)
+print(math.floor(3.65))
+print(math.cos(3.6))
+print(type(msg))
+print(msg.capitalize())
+print(msg.find("e"))
+print(msg.upper()) ##print in uppercase
+print(msg.lower()) ##print in lowercase
+
+items = [1,2,3, "mah"] ##list in python
+print(items)
+print(type(items))
