6aebdaf377228051e2923b94d3f53423e17f42f8
MahRukh <mahrukhdev@gmail.com>
2024-02-07 09:37:26 +0500

    expression

---
+++ Py4e.py
@@ -0,0 +1,15 @@
+#print("Hello World")
+
+x = 1
+print(x)
+x = x + 1
+print(x)
+
+#numeric expressions
+num = 500
+sum = num + 2
+multiply = num * 2
+divide = num / 2
+remainder = num % 5
+power = 2 ** 3 
+ 
